f977f44481de8ea15a26afd8c80b770ac305d788
Natan Merhawi <natanmerhawitafla@gmail.com>
2024-09-11 08:08:24 +0300

    Some Adjustments

--- Com/Vehicle/Test/Main.java
+++ Com/Vehicle/Test/Main.java
@@ -5,18 +5,18 @@ import Com.Vehicle.ElectricCar;
 public class Main {
 
     public static void main(String[] args) {
-        Car c1 = new Car();
-        c1.setSpeed(50);  
-        c1.setFuelLevel(50);  
+        Car c = new Car();
+        c.setSpeed(70);  
+        c.setFuelLevel(70);  
         System.out.println("TESTING CAR");
-        System.out.println(c1.getSpeedMph());
-        System.out.println(c1.getDistancekm());
+        System.out.println(c.getSpeedMph());
+        System.out.println(c.getDistancekm());
 
         System.out.println("TESTING ELECTRIC CAR");
-        ElectricCar Ec1 = new ElectricCar();
-        Ec1.setBatteryLevel(10);  
-        Ec1.drive(50); 
-        System.out.println(Ec1.getBatteryLevel());
-        System.out.println(Ec1.getBatteryLevel());
+        ElectricCar Ec = new ElectricCar();
+        Ec.setBatteryLevel(10);  
+        Ec.drive(40); 
+        System.out.println(Ec.getBatteryLevel());
+        System.out.println(Ec.getBatteryLevel());
     }
 }
